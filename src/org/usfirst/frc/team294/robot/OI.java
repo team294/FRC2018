66bb79fd73d487a82e12f517866985cccd3014ce
GrantS2 <Team294@teams.org>
2018-01-20 14:17:25 -0800

    Added shift High and Low commands

--- src/org/usfirst/frc/team294/robot/OI.java
+++ src/org/usfirst/frc/team294/robot/OI.java
@@ -1,18 +1,24 @@
 package org.usfirst.frc.team294.robot;
 
-import org.usfirst.frc.team294.robot.commands.PnuematicShift;
+import org.usfirst.frc.team294.robot.commands.ShiftLow;
+import org.usfirst.frc.team294.robot.commands.ShiftHigh;
 import org.usfirst.frc.team294.robot.commands.DriveWithJoystick;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class OI {
-	public static Joystick leftJoystick = new Joystick(0); //Left Joystick is in port 0
+	public static Joystick leftJoystick = new Joystick(0); // Left Joystick is in port 0
 
-	public static Joystick rightJoystick = new Joystick(1); //Right Joystick is in port 1
+	public static Joystick rightJoystick = new Joystick(1); // Right Joystick is in port 1
 
 	{
-		SmartDashboard.putData("Start Drive Train", new DriveWithJoystick()); //Adds a start Button
-		SmartDashboard.putData("Start Shifter", new PnuematicShift()); //Adds a start Button
+		SmartDashboard.putData("Start Drive Train", new DriveWithJoystick()); // Adds a start Button
+		Button leftTrigger = new JoystickButton(leftJoystick, 1);
+		Button rightTrigger = new JoystickButton(rightJoystick, 1);
+		leftTrigger.whenPressed(new ShiftLow());
+		rightTrigger.whenPressed(new ShiftHigh());
 	}
 }
