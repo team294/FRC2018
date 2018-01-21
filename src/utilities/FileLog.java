47013becb9e301f0f9e1c7719398059a32e8cc2a
BCR <BCR@rbusd.edu>
2018-01-21 15:19:24 -0800

    created FileLog
    created FileLog class inside a package called utilities to log
    information from a match

---
+++ src/utilities/FileLog.java
@@ -0,0 +1,71 @@
+package utilities;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+public class FileLog {
+	
+	private FileWriter fileWriter;
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+	/**
+	 * Creates a new log file called "/home/lvuser/logfile.date.time.txt"
+	 */
+	public FileLog() {
+		this("/home/lvuser/logfile");
+	}
+	
+	/**
+	 * Creates a new log file.
+	 * @param filename Path and name of log file.  ".date.time.txt" will automatically be added to the end of the file name.
+	 */
+	public FileLog(String filename) {
+		final SimpleDateFormat fileDateFormat = new SimpleDateFormat("yyyy-MM-dd.HH-mm-ss");
+
+		try {
+			fileWriter = new FileWriter(filename + "." + fileDateFormat.format(System.currentTimeMillis()) + ".txt", true);
+			fileWriter.write("----------------------------\n");
+			fileWriter.write(dateFormat.format(System.currentTimeMillis()) + ",  FileLog open.\n");
+			fileWriter.flush();
+		} catch (IOException exception) {
+			System.out.println("Could not open log file: " + exception);
+		}
+	}
+	
+	/**
+	 * Writes a message to the log file.  The message will be timestamped.  Does not echo the message to the screen.
+	 * @param msg
+	 */
+	public void writeLog(String msg) {
+		if (fileWriter == null) return;
+		try {
+			fileWriter.write(dateFormat.format(System.currentTimeMillis()) + ", " + msg + "\n");
+			fileWriter.flush();
+		} catch (IOException exception) {
+		}
+	}
+	
+	/**
+	 * Writes a message to the log file.  The message will be timestamped.  Also echos the message to the screen.
+	 * @param msg
+	 */
+	public void writeLogEcho(String msg) {
+		writeLog(msg);
+		System.out.println("Log: " + msg);
+	}
+	
+	/**
+	 * Closes the log file.  All writes after closing the log file will be ignored.
+	 */
+	public void close() {
+		try {
+			fileWriter.close();
+			fileWriter = null;
+		} catch (IOException exception) {
+		}
+	}
+	
+	
+
+}
