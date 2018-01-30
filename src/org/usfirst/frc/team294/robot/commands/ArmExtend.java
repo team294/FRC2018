1708e091f7b9e9731150a8aebf31e1baf346b11a
Team 294 <team294@email.edu>
2018-01-29 20:41:41 -0800

    Added major/minor pistons
    Piston hardware, limit switch reading from digital IO pins, state
    tracking in software, and get/set methods

--- src/org/usfirst/frc/team294/robot/commands/ArmExtend.java
+++ src/org/usfirst/frc/team294/robot/commands/ArmExtend.java
@@ -17,7 +17,7 @@ public class ArmExtend extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.protoArmPiston.extendPiston();
+    	Robot.protoArmPiston.extendMajorPiston();
     	
     }
 
