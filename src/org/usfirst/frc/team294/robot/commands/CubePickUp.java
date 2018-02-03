dce00922e3ac583edc9c23ac4fd308f70687dc5c
BCR <BCR@rbusd.edu>
2018-02-03 13:12:10 -0800

    added wait

--- src/org/usfirst/frc/team294/robot/commands/CubePickUp.java
+++ src/org/usfirst/frc/team294/robot/commands/CubePickUp.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team294.robot.commands;
 import org.usfirst.frc.team294.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
-//import java.util.concurrent.*;
 
 /**
  *
@@ -11,41 +10,41 @@ import edu.wpi.first.wpilibj.command.Command;
 public class CubePickUp extends Command {
 
 	private double percent;
-	
-    public CubePickUp(double percent) {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.claw);
-    	this.percent = percent;
-    }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	Robot.claw.setClawMotorToPercentPower(percent);
-    	Robot.claw.extendPiston();
-    }
+	public CubePickUp(double percent) {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.claw);
+		this.percent = percent;
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	if (Robot.inputs.isObjectPresent()) {
-    		Robot.claw.retractPiston();
-    		//wait(250);
-    		Robot.claw.setClawMotorToPercentPower(0);
-    	}
-    	
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.claw.setClawMotorToPercentPower(percent);
+		Robot.claw.extendPiston();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if (Robot.inputs.isObjectPresent()) {
+			Robot.claw.retractPiston();
+			setTimeout(250);
+			Robot.claw.setClawMotorToPercentPower(0);
+		}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
