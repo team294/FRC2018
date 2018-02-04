a7068d6383748c05318262c762fb9c1308ec4455
Ethan Ferguson <Team294@teams.org>
2018-02-03 18:15:36 -0800

    saved changes

--- src/org/usfirst/frc/team294/robot/commands/ArmMoveToAndExtend1.java
+++ src/org/usfirst/frc/team294/robot/commands/ArmMoveToAndExtend1.java
@@ -14,35 +14,19 @@ public class ArmMoveToAndExtend1 extends CommandGroup {
 	double currentAngle = Robot.protoArmMotor.getArmDegrees();
 
 	public ArmMoveToAndExtend1(double destAngle, boolean finalPistonPosition) {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
+		requires(Robot.protoArmMotor);
+		addSequential(new ArmMoveToLegalRange());
+		if (RobotMap.getArmZone(currentAngle) == RobotMap.getArmZone(destAngle)) {
+			addParallel(new ArmMoveToDestAngle(destAngle));
+			if (finalPistonPosition) {
+				addSequential(new ArmPistonSmartExtend());
+			}
+		} else {
+			addParallel(new ArmMoveToEdge(destAngle));
+			addSequential(new ArmPistonRetractBoth());
+			addParallel(new ArmMoveToDestAngle(destAngle));
+			addSequential(new ArmPistonSmartExtendInDestZone(destAngle));
 
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-    	requires(Robot.protoArmMotor);
-    	addSequential(new ArmMoveToLegalRange());
-    	if (RobotMap.getArmZone(currentAngle) == RobotMap.getArmZone(destAngle)) {
-    		addParallel(new ArmMoveToDestAngle(destAngle));
-    		if(finalPistonPosition) {
-    		addSequential(new ArmPistonSmartExtend());
-    		}
-    		}
-    	else {
-    		addParallel(new ArmMoveToEdge(destAngle));
-    		//addSequential();
-    		
-    		
-    	}
-    }
+		}
+	}
 }
