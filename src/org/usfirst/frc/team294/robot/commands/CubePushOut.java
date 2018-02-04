e25c26ed80d347a4fa01c94e10ccf721c6536aea
BCR <BCR@rbusd.edu>
2018-02-03 16:56:42 -0800

    Made CubePushOut stop when pressed on SmartDashboard

--- src/org/usfirst/frc/team294/robot/commands/CubePushOut.java
+++ src/org/usfirst/frc/team294/robot/commands/CubePushOut.java
@@ -26,19 +26,23 @@ public class CubePushOut extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.claw.openClaw();
+    	Robot.claw.setClawMotorToPercentPower(leftPercent, rightPercent);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.claw.setClawMotorToPercentPower(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.claw.setClawMotorToPercentPower(0, 0);
     }
 }
