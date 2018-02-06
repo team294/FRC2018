515c0452b357d082bce015814e4eafd695c49848
Rob Coe <robcoe18@gmail.com>
2018-02-05 16:11:37 -0800

    Fixed some errors that got messed up in merge
    I am not sure why, but some autonomous code got deleleted in the merge.
    I manually cut and pasted it back, but it needs to be checked.

--- src/org/usfirst/frc/team294/robot/OI.java
+++ src/org/usfirst/frc/team294/robot/OI.java
@@ -7,7 +7,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-
+import org.usfirst.frc.team294.robot.commands.autoroutines.*;
+import org.usfirst.frc.team294.robot.commands.TurnGyro.Units;
 	
 
 
@@ -23,18 +24,21 @@ public class OI {
 
 	public OI() {
 		// Initialize our auto plan chooser
-		chooser_autoPlan.addDefault("Closest, far/far = scale", 0);
-		chooser_autoPlan.addObject("Closest, far/far = switch front", 1);
-		chooser_autoPlan.addObject("Closest, far/far = switch back", 2);
-		chooser_autoPlan.addObject("Scale only", 3);
-		chooser_autoPlan.addObject("Switch only (middle)", 4);
+		chooser_autoPlan.addDefault("do Closest, if both far do scale", 0);
+		chooser_autoPlan.addObject("do Closest, if both far do switch from front", 1);
+		chooser_autoPlan.addObject("do closest, if both far do switch from back", 2);
+		chooser_autoPlan.addObject("do Scale only", 3);
+		chooser_autoPlan.addObject("do Switch only from middle", 4);
+		chooser_autoPlan.addObject("Go to baseline", 5);
 		SmartDashboard.putData("Auto Plan Selection", chooser_autoPlan);
+		SmartDashboard.putData("AutoTest1",new AutoTest1());
 		
 		// Initialize our position chooser
 		chooser_startPosition.addDefault("- choose from below -", 0);
 		chooser_startPosition.addObject("Left", 1);
 		chooser_startPosition.addObject("Middle", 2);
 		chooser_startPosition.addObject("Right", 3);
+		
 		SmartDashboard.putData("Start Position Selection", chooser_startPosition);
 	
 		SmartDashboard.putData("Start Drive Train", new DriveWithJoystick()); // Adds a start Button
@@ -52,6 +56,11 @@ public class OI {
 		Button rightTrigger = new JoystickButton(rightJoystick, 1);
 		leftTrigger.whenPressed(new ShiftLow());
 		rightTrigger.whenPressed(new ShiftHigh());
+		
+		SmartDashboard.putData("Turn heckla small", new TurnGyro(90, Units.Degrees));
+		SmartDashboard.putData("DriveStraightDistanceProfile", new DriveStraightDistanceProfile(24, 0));
+		SmartDashboard.putNumber("DistToTravelDSDG", 150);
+		SmartDashboard.putData(" ProfileTest", new DriveStraightDistanceEllipse(100, 1000, 0));
 		
 		SmartDashboard.putData("Photo Switch", new ReadPhotoSwitch()); // For use with the intake
 		SmartDashboard.putData("Pick Up Cube", new CubePickUp());
