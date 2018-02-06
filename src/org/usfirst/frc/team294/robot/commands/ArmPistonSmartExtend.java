41c21a894f7b0af25b184052bcddaa243e13c1bf
Team 294 <team294@email.edu>
2018-02-05 20:05:55 -0800

    Fixed errors

--- src/org/usfirst/frc/team294/robot/commands/ArmPistonSmartExtend.java
+++ src/org/usfirst/frc/team294/robot/commands/ArmPistonSmartExtend.java
@@ -36,21 +36,24 @@ public class ArmPistonSmartExtend extends Command {
 	protected void execute() {
 	}
 
-	// Make this return true when this Command no longer needs to run execute()
+	// Make this return true when this Command no longer needs to run execute
 	protected boolean isFinished() {
 		double currentAngle = Robot.protoArmMotor.getArmDegrees();
 		if (currentAngle <= RobotMap.lowerBound && currentAngle >= RobotMap.minAngle) {
-			return (Robot.protoArmPiston.getMajor() == PistonPositions.Retracted)&& (Robot.protoArmPiston.getMinor()== PistonPositions.Extended);
+			return (Robot.protoArmPiston.getMajor() == PistonPositions.Retracted)
+					&& (Robot.protoArmPiston.getMinor() == PistonPositions.Extended);
 		} else if (currentAngle <= RobotMap.upperBound && currentAngle >= RobotMap.middleBound) {
-			return (Robot.protoArmPiston.getMajor() == PistonPositions.Extended)&& (Robot.protoArmPiston.getMinor()== PistonPositions.Extended);
+			return (Robot.protoArmPiston.getMajor() == PistonPositions.Extended)
+					&& (Robot.protoArmPiston.getMinor() == PistonPositions.Extended);
 		} else {
-			return (Robot.protoArmPiston.getMajor() == PistonPositions.Retracted)&& !(Robot.protoArmPiston.getMinor()== PistonPositions.Retracted);
+			return (Robot.protoArmPiston.getMajor() == PistonPositions.Retracted)
+					&& !(Robot.protoArmPiston.getMinor() == PistonPositions.Retracted);
 		}
 	}
 
 	// Called once after isFinished returns true
 	protected void end() {
-		
+
 	}
 
 	// Called when another command which requires one or more of the same
