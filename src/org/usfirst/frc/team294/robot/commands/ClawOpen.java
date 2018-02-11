00d7e654d025723f57ecd38af9b7dc459b01eee5
Ethan Ferguson <ethanf108@gmail.com>
2018-02-11 15:45:06 -0800

    Added commands to manually open/close claw and keep out zone

---
+++ src/org/usfirst/frc/team294/robot/commands/ClawOpen.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team294.robot.commands;
+
+import org.usfirst.frc.team294.robot.Robot;
+import org.usfirst.frc.team294.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ClawOpen extends Command {
+		
+	/**
+	 * Manually opens claw, but not if claw is in keep-out zone
+	 */
+    public ClawOpen() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.claw);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+		Robot.claw.openClaw();
+	}
+  
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
