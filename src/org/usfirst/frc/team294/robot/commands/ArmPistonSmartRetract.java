f88789fb6b91c7a066924bb71f018696d4b56a83
Team 294 <team294@email.edu>
2018-02-15 20:43:15 -0800

    Readded  Matt's Folly

---
+++ src/org/usfirst/frc/team294/robot/commands/ArmPistonSmartRetract.java
@@ -0,0 +1,62 @@
+package org.usfirst.frc.team294.robot.commands;
+
+import org.usfirst.frc.team294.robot.Robot;
+import org.usfirst.frc.team294.robot.RobotMap;
+import org.usfirst.frc.team294.robot.RobotMap.PistonPositions;
+import org.usfirst.frc.team294.robot.RobotMap.ArmZones;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ArmPistonSmartRetract extends Command {
+	boolean pistonFinal;
+	boolean retract;
+	double destAngle;
+
+	public ArmPistonSmartRetract(double destAngle, boolean pistonFinal) {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.armPiston);
+		this.pistonFinal = pistonFinal;
+		this.destAngle = destAngle;
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		this.retract = false;
+		ArmZones currentZone = RobotMap.getArmZone(Robot.armMotor.getArmDegrees());
+		ArmZones destZone = RobotMap.getArmZone(destAngle);
+		if (currentZone == ArmZones.Middle || currentZone == ArmZones.Backwards 
+				|| currentZone != destZone || !pistonFinal) {
+			// If arm is in the middle or backwards zone set pistons to retracted
+			Robot.armPiston.smartRetract();
+			retract = true;
+		}
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		if (!retract)
+			return true;
+		else if ( Robot.armPiston.getMajor() == PistonPositions.Retracted)
+			// && Robot.protoArmPiston.getMinor() == PistonPositions.Retracted)
+			return true;
+		return false; // According to Liam, the todo is done
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
