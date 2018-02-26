cc2acacfeef8a64e083b9b298f10421e706914d1
a <team294@rbusd.org>
2018-02-25 17:25:04 -0800

    Added Some Controls stuff (Xbox)

--- src/org/usfirst/frc/team294/robot/OI.java
+++ src/org/usfirst/frc/team294/robot/OI.java
@@ -153,22 +153,22 @@ public class OI {
 		//xbB[1].whenPressed(new Command()); // Lower intake mechanism
 		//xbB[2].whenPressed(new PistonCommand()); // Arm Piston actuation
 		//xbB[3].whenPressed(new PistonCommand()); // Arm Piston actuation
-		//xbB[4].whenPressed(new Command()); // Raise intake mechanism
-		xbB[5].whenPressed(new LoadCubeSequence()); // Intake sequence
-		xbB[6].whenPressed(new PassiveOuttake()); // Outtake
+		xbB[4].whenPressed(new ClawSetState(true)); // Open Claw
+		//xbB[5].whenPressed(new LoadCubeSequence()); // Intake sequence
+		xbB[6].whenPressed(new LoadCubeSequence()); // Intake Sequence
 		//xbB[7].whenPressed(new ClimbCommand()); // Reserved for climbing
 		//xbB[8].whenPressed(new ClimbCommand()); // Reserved for climbing
-		xbB[9].whenPressed(new StopIntakeAndClaw()); // Stop flywheels
-		//xbB[10].whenPressed(new OverrideCommand()); // Override climb OR arm
+		//xbB[9].whenPressed(new OverrideCommand()); // Override climb OR arm
+		xbB[10].toggleWhenPressed(new ArmMotorControlJoystick()); // Manual Arm Control
 
-		xbPovUp.whenActive(new ArmMoveWithPiston(ArmPositions.ScaleHigh)); // Arm to scale backwards
-		xbPovDown.whenActive(new ArmMoveWithPiston(ArmPositions.Intake)); // Arm to intake position
+		xbPovUp.whenActive(new ArmMoveWithPiston(ArmPositions.Intake));  // Arm to intake position 
+		xbPovDown.whenActive(new ArmMoveWithPiston(ArmPositions.ScaleHigh)); // Arm to scale backwards
 		xbPovLeft.whenActive(new ArmMoveWithPiston(ArmPositions.Switch)); // Arm to switch position
-		xbPovRight.whenActive(new ArmMoveWithPiston(ArmPositions.ScaleLow)); // Arm to alternate scale position
+		xbPovRight.whenActive(new ArmMoveWithPiston(ArmPositions.ScaleLow)); // Arm to scale fowards
 
 		// Xbox triggers
-		//xbLT.whenActive(new Command()); // Prepare to score cube (rev up flywheels), alternate climb/arm override
-		//xbRT.whenActive(new Command()); // Score cube
+		xbLT.whenActive(new ArmPistonSmartExtend()); // Prepare to score cube (rev up flywheels), alternate climb/arm override
+		xbRT.whenActive(new CubeShootOut()); // Score cube
 
 		Button armButton2 = new JoystickButton(armJoystick,2);
 		Button armButton3 = new JoystickButton(armJoystick,3);
