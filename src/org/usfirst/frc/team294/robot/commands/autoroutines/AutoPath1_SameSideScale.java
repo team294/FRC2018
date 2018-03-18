d6769fafa23682c342768ed4489a6da8495dfe89
team <bcr@bcr.org>
2018-03-18 13:35:31 -0700

    Vision auto test #1

--- src/org/usfirst/frc/team294/robot/commands/autoroutines/AutoPath1_SameSideScale.java
+++ src/org/usfirst/frc/team294/robot/commands/autoroutines/AutoPath1_SameSideScale.java
@@ -16,9 +16,9 @@ public class AutoPath1_SameSideScale extends CommandGroup {
 	public AutoPath1_SameSideScale(StartingPosition startPosition) {
 		int angleMultiplier = 1;
 		switch (startPosition) {
-		
+
 		case Left:
-			 angleMultiplier = 1;
+			angleMultiplier = 1;
 			break;
 		case Right:
 			angleMultiplier = -1;
@@ -29,32 +29,32 @@ public class AutoPath1_SameSideScale extends CommandGroup {
 		addParallel(new ClawSetMotorSpeed(-0.40));
 		addSequential(new WaitCommand(0.1));
 		addParallel(new ArmMoveWithIntakeBack());
-		addSequential(new DriveStraightDistanceProfile(-246, 6*angleMultiplier, 100, 100));
+		addSequential(new DriveStraightDistanceProfile(-246, 6 * angleMultiplier, 100, 100));
 		addParallel(new ArmMoveWithPiston(RobotMap.armScaleBackwardsPos, true));
 		addSequential(new TurnGyro(35 * angleMultiplier, TurnGyro.Units.Degrees));
-	//	addSequential(new WaitCommand(0.2));
+		// addSequential(new WaitCommand(0.2));
 		addSequential(new CubeShootOut());
-		addSequential( new WaitCommand(1));
-		addSequential( new ArmMoveWithPiston(-55, false));
-		addSequential( new TurnGyro( 135 * angleMultiplier, TurnGyro.Units.Degrees ));
-		addSequential( new TurnGyro());
-		addSequential(new DriveStraightDistanceProfile( -60, Robot.driveTrain.getGyroRotation(), 95, 150));
+		addSequential(new WaitCommand(.5));
+		addSequential(new ArmMoveWithPiston(-55, false));
+		addSequential(new TurnGyro(135 * angleMultiplier, TurnGyro.Units.Degrees));
+		addSequential(new TurnGyro());
+		addSequential(new DriveStraightDistanceProfile(-60, Robot.driveTrain.getGyroRotation(), 95, 150));
 		addSequential(new IntakeCube());
-		addSequential(new DriveStraightDistanceProfile( 60, 45 * angleMultiplier, 95, 150));
-		addParallel(new ArmMoveWithPiston(110, true));
+		addSequential(new DriveStraightDistanceProfile(60, Robot.driveTrain.getGyroRotation(), 95, 150));
+		addParallel(new ArmMoveWithPiston(RobotMap.armScaleBackwardsPos, true));
 		addSequential(new CubeShootOut());
-		
+
 		// Score cube 1
-		
-//		addSequential(new TurnGyro(-45 * angleMultiplier, TurnGyro.Units.Degrees));
-//		addSequential(new DriveStraightDistanceProfile(30, -45 * angleMultiplier));
-//		// TODO We should TurnGyro before going from 45 degrees to 0 degrees
-//		addSequential(new DriveStraightDistanceProfile(15, 0));  
-//		// TODO We should TurnGyro before going from 0 degrees to 45 degrees
-//		
-//		// Grab second cube
-//		
-//		addSequential(new DriveStraightDistanceProfile(-20, -45 * angleMultiplier));
-//		addSequential(new DriveStraightDistanceProfile(-60, 45 * angleMultiplier ));
+
+		// addSequential(new TurnGyro(-45 * angleMultiplier, TurnGyro.Units.Degrees));
+		// addSequential(new DriveStraightDistanceProfile(30, -45 * angleMultiplier));
+		// // TODO We should TurnGyro before going from 45 degrees to 0 degrees
+		// addSequential(new DriveStraightDistanceProfile(15, 0));
+		// // TODO We should TurnGyro before going from 0 degrees to 45 degrees
+		//
+		// // Grab second cube
+		//
+		// addSequential(new DriveStraightDistanceProfile(-20, -45 * angleMultiplier));
+		// addSequential(new DriveStraightDistanceProfile(-60, 45 * angleMultiplier ));
 	}
 }
