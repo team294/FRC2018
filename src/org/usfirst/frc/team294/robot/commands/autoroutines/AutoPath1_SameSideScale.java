3ed4772c84d1a9ee332bbf0bd6063dbca807f501
team <bcr@bcr.org>
2018-03-17 17:03:30 -0700

    2 cube scale attempt number 1

--- src/org/usfirst/frc/team294/robot/commands/autoroutines/AutoPath1_SameSideScale.java
+++ src/org/usfirst/frc/team294/robot/commands/autoroutines/AutoPath1_SameSideScale.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team294.robot.commands.autoroutines;
 
+import org.usfirst.frc.team294.robot.Robot;
 import org.usfirst.frc.team294.robot.RobotMap;
 import org.usfirst.frc.team294.robot.commands.*;
 import org.usfirst.frc.team294.utilities.AutoSelection.StartingPosition;
@@ -30,8 +31,17 @@ public class AutoPath1_SameSideScale extends CommandGroup {
 		addParallel(new ArmMoveWithIntakeBack());
 		addSequential(new DriveStraightDistanceProfile(-246, 6*angleMultiplier, 100, 100));
 		addParallel(new ArmMoveWithPiston(RobotMap.armScaleBackwardsPos, true));
-		addSequential(new TurnGyro(30 * angleMultiplier, TurnGyro.Units.Degrees));
-		addSequential(new WaitCommand(0.25));
+		addSequential(new TurnGyro(35 * angleMultiplier, TurnGyro.Units.Degrees));
+	//	addSequential(new WaitCommand(0.2));
+		addSequential(new CubeShootOut());
+		addSequential( new WaitCommand(1));
+		addSequential( new ArmMoveWithPiston(-55, false));
+		addSequential( new TurnGyro( 135 * angleMultiplier, TurnGyro.Units.Degrees ));
+		addSequential( new TurnGyro());
+		addSequential(new DriveStraightDistanceProfile( -60, Robot.driveTrain.getGyroRotation(), 95, 150));
+		addSequential(new IntakeCube());
+		addSequential(new DriveStraightDistanceProfile( 60, 45 * angleMultiplier, 95, 150));
+		addParallel(new ArmMoveWithPiston(110, true));
 		addSequential(new CubeShootOut());
 		
 		// Score cube 1
