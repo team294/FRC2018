51fc8fea18be3b261c410cb82a2d70b04e963f36
Don Sawdai <team@rbusd.org>
2018-03-17 19:16:46 -0700

    Commented out one check (for now)

--- src/org/usfirst/frc/team294/robot/subsystems/ArmMotor.java
+++ src/org/usfirst/frc/team294/robot/subsystems/ArmMotor.java
@@ -55,7 +55,6 @@ public class ArmMotor extends Subsystem {
 	private ArmProfileGenerator trapezoid;
 
 	public ArmMotor() {
-		previousArmAngle = getArmDegrees();
 		// armMotor.set(ControlMode.Position, 3);
 		armMotor1.set(ControlMode.PercentOutput, 0);
 		armMotor2.set(ControlMode.Follower, RobotMap.armMotor1);
@@ -87,6 +86,9 @@ public class ArmMotor extends Subsystem {
 		armMotor1.configClosedloopRamp(0.25, 10);
 		armMotor1.configPeakOutputForward(MAX_UP_PERCENT_POWER, 10);
 		armMotor1.configPeakOutputReverse(MAX_DOWN_PERCENT_POWER, 10);
+
+		// Set up PID and watchdogs
+		previousArmAngle = getArmDegrees();
 		trapezoid = new ArmProfileGenerator(getArmDegrees(), getArmDegrees(), 0, 0, 0);
 		lastTime = System.currentTimeMillis();
 		
@@ -365,23 +367,24 @@ public class ArmMotor extends Subsystem {
 
 	public void periodic() {
 		updateSmartDashboard();
-		// Set armCalZero, if not already set, by using known value of lower limit
-		// switch
+		// Set armCalZero, when limit switch is hit [(was) if not already set], by using known value of lower limit switch
 		//if (!Robot.robotPrefs.armCalibrated) {
-			SensorCollection sc = armMotor1.getSensorCollection();
-			if (sc.isRevLimitSwitchClosed()) {
-				Robot.log.writeLogEcho("Arm auto cal pre,target angle," + finalAngle + ",current angle," + getArmDegrees() 
-					+ ",arm raw enc," + getArmEncRaw() + ",arm cal zero," + Robot.robotPrefs.armCalZero);
-				Robot.robotPrefs.setArmCalibration( getArmEncRaw() - (RobotMap.minAngle * TICKS_PER_DEGREE), false);
-				Robot.log.writeLogEcho("Arm auto cal post,target angle," + finalAngle + ",current angle," + getArmDegrees() 
-				+ ",arm raw enc," + getArmEncRaw() + ",arm cal zero," + Robot.robotPrefs.armCalZero);
-			}
-			
-		//}
-		//the following two boolean statement checks for sudden jumps in arm degree value as well as whether the arm is inside a reasonable boundary.
-		if((previousArmAngle - getArmDegrees())> 50) {
-			Robot.robotPrefs.armCalibrated = false;
+		SensorCollection sc = armMotor1.getSensorCollection();
+		if (sc.isRevLimitSwitchClosed()) {
+			Robot.log.writeLogEcho("Arm auto cal pre,target angle," + finalAngle + ",current angle," + getArmDegrees() 
+			+ ",arm raw enc," + getArmEncRaw() + ",arm cal zero," + Robot.robotPrefs.armCalZero);
+			Robot.robotPrefs.setArmCalibration( getArmEncRaw() - (RobotMap.minAngle * TICKS_PER_DEGREE), false);
+			Robot.log.writeLogEcho("Arm auto cal post,target angle," + finalAngle + ",current angle," + getArmDegrees() 
+			+ ",arm raw enc," + getArmEncRaw() + ",arm cal zero," + Robot.robotPrefs.armCalZero);
 		}
+		//}
+			
+		// The following boolean statement checks for sudden jumps in arm degree value.
+		// Need to verify:  What happens if we just calibrated the arm?  Will previousArmAngle be meaningless, then we trigger this?
+//		if((previousArmAngle - getArmDegrees())> 50) {
+//			Robot.robotPrefs.armCalibrated = false;
+//		}
+		//the following boolean statement checks whether the arm is inside a reasonable boundary.
 		if(getArmDegrees() > 140 || getArmDegrees() < -58) {
 			Robot.robotPrefs.armCalibrated = false;
 		}
