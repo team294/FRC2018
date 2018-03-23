102ee3a371ac377333d8fa28c5848bd8a2b86ddf
team <team@rbusd.org>
2018-03-23 14:46:35 -0700

    Test code before Match 44 -- Scale 2cube auto with rotate cube

--- src/org/usfirst/frc/team294/robot/commands/autoroutines/AutoPath1_SameSideScale.java
+++ src/org/usfirst/frc/team294/robot/commands/autoroutines/AutoPath1_SameSideScale.java
@@ -2,10 +2,12 @@ package org.usfirst.frc.team294.robot.commands.autoroutines;
 
 import org.usfirst.frc.team294.robot.Robot;
 import org.usfirst.frc.team294.robot.RobotMap;
+import org.usfirst.frc.team294.robot.RobotMap.ArmZones;
 import org.usfirst.frc.team294.robot.commands.*;
 import org.usfirst.frc.team294.utilities.AutoSelection.StartingPosition;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.ConditionalCommand;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
@@ -52,6 +54,27 @@ public class AutoPath1_SameSideScale extends CommandGroup {
 		addSequential(new DriveStraightDistanceProfile(33, 0, 100, 100));
 		addSequential(new WaitCommand(.75));
 
+		// If we have the cube in the intake in diamond shape (not in claw), then try rotating the cube
+    	addSequential(new ConditionalCommand(new IntakeInvertAndGrab()) {
+			protected boolean condition() {
+				return (Robot.intake.getPhotoSwitch() && !Robot.claw.getPhotoSwitch());
+			}
+		});
+		// If we have the cube in the intake in diamond shape (not in claw), then try rotating the cube
+    	addSequential(new ConditionalCommand(new IntakeInvertAndGrab()) {
+			protected boolean condition() {
+				return (Robot.intake.getPhotoSwitch() && !Robot.claw.getPhotoSwitch());
+			}
+		});
+
+		// If we have the cube in the claw, then go back to scale and score
+    	addSequential(new ConditionalCommand(new AutoPath1_part2_Score2ndCube(startPosition)) {
+			protected boolean condition() {
+				return (Robot.claw.getPhotoSwitch());
+			}
+		});
+    		
+/* Moved this to AutoPath1_part2_Score2ndCube()
 		// Assume we have the cube, so go back to scale to score
 		addParallel(new ArmMoveWithPiston(RobotMap.armScaleBackwardsPos, false));//true));
 		addSequential(new DriveStraightDistanceProfile(-48, 0 * angleMultiplier, 100, 100));
@@ -61,6 +84,6 @@ public class AutoPath1_SameSideScale extends CommandGroup {
 		// Leave arm up, ready for teleop
 //		addSequential(new ArmMoveWithPiston(RobotMap.armScaleLowPos, false));//true)); // enforce the arm being up before shooting
 		addSequential(new ArmMoveWithPiston(90, false));//true)); // move arm to +90, since the arm will stay there when disabled
-
+*/
 	}
 }
