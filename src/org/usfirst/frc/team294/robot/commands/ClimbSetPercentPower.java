7068fea1da80fa84bb89b1deb494eea41a7e27e2
team <team@294.com>
2018-04-03 19:02:21 -0700

    Added Manual Climb Button

--- src/org/usfirst/frc/team294/robot/commands/ClimbSetPercentPower.java
+++ src/org/usfirst/frc/team294/robot/commands/ClimbSetPercentPower.java
@@ -28,7 +28,7 @@ public class ClimbSetPercentPower extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true
@@ -38,5 +38,6 @@ public class ClimbSetPercentPower extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.climb.setClimbMotors(0);
     }
 }
