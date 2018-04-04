fd3a4c32e3afbf3f9e841c2906d636f155196d10
team <team@294.com>
2018-04-03 19:11:39 -0700

    oof messed it up again

--- src/org/usfirst/frc/team294/robot/commands/ClimbSetPercentPower.java
+++ src/org/usfirst/frc/team294/robot/commands/ClimbSetPercentPower.java
@@ -45,5 +45,6 @@ public class ClimbSetPercentPower extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.climb.setClimbMotors(0);
     }
 }
